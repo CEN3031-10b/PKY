5c1099751cf6ddbaa076f94511e0a82aba5905c0
Michael Rodriguez <michaelrod@gmail.com>
2016-04-12 13:09:50 -0400

    heroku testing 5

--- config/assets/production.js
+++ config/assets/production.js
@@ -19,7 +19,7 @@ module.exports = {
         'public/lib/angular-ui-utils/ui-utils.min.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
         'public/lib/angular-file-upload/dist/angular-file-upload.js',
-        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js'
+        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
 		'public/lib/angular-filter/dist/angular-filter.js',
 		'public/lib/mathquill-0.10.1/mathquill.js',
 		'public/lib/tr-ng-grid/trNgGrid.js',
