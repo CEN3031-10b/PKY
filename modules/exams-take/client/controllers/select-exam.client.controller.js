781063a453751262e87953a68ea704f1499e0a10
Michael Rodriguez <michaelrod@gmail.com>
2016-04-15 15:48:36 -0400

    added functions to select exam controller

--- modules/exams-take/client/controllers/select-exam.client.controller.js
+++ modules/exams-take/client/controllers/select-exam.client.controller.js
@@ -14,9 +14,40 @@
 	$scope.error = null;
 	$scope.selected_mode = "take";
 	
+	// from edit exams page
+	$scope.calculate_points = function(_exam){
+		_exam.point_total = 0;
+		for(var i = 0; i <_exam.questions.length; ++i){
+			if(_exam.questions[i].points != null){
+				_exam.point_total += Number(_exam.questions[i].points);
+			}
+			else{
+				_exam.point_total = 'One or more questions with undefined point value.';
+				break;
+			}
+		}
+	};	
+	
+	// set point_total attribute of each exam
+	for(var i = 0; i < $scope.exams.length; ++i){
+		$scope.calculate_points($scope.exams[i]);
+	}
+
+	$scope.calculate_remaining_attempts = function(_attempts, _exams){
+		for(var i = 0; i < _exams.length; ++i){
+			_exams[i].remaining_attempts = _exams[i].allowed_attempts;
+			for(var j = 0; j < _attempts.length; ++j){
+				if(_exams[i]._id === _attempts[j].exam._id && _attempts[j].submitted){
+					_exams[i].remaining_attempts--;
+				}
+			}
+		}
+	};
+	
 	ExamsAnalysisService.get_attempts()
 	.then(function(response){
 		$scope.attempts = response.data;
+		$scope.calculate_remaining_attempts($scope.attempts, $scope.exams); 
 	}, function(error){
 		$scope.error = error;
 	});
